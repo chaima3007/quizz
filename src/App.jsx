a04d2bf4492a6741ded894781196cc4da3f7296a
chaima3007 <chaimaagheriballah@gmail.com>
2025-02-10 16:02:14 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import Quiz from './components/Quiz';
-import Home from './components/Home';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+import Result from "./components/Result";
 
-function App() {
-	return (
-		<>
-			<Home />
-			<Quiz
-				endQuiz={() => {
-					console.log('end quizz');
-				}}
-			/>
-		</>
-	);
-}
+const App = () => {
+  const [step, setStep] = useState("home");
+  const [score, setScore] = useState(0);
+
+  const startQuiz = () => setStep("quiz");
+  const endQuiz = (finalScore) => {
+    setScore(finalScore);
+    setStep("result");
+  };
+  const restartQuiz = () => setStep("home");
+
+  return (
+    <div className="app-container">
+      {step === "home" && <Home startQuiz={startQuiz} />}
+      {step === "quiz" && <Quiz endQuiz={endQuiz} />}
+      {step === "result" && <Result score={score} restartQuiz={restartQuiz} />}
+    </div>
+  );
+};
 
 export default App;
